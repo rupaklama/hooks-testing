a5740db4dd15c2c885af9649c7cd47387715e6c2
Rupak <rupaklamapythondeveloper@gmail.com>
2020-12-02 08:06:15 -0800

    test updated

--- src/Component.test.js
+++ src/Component.test.js
@@ -10,12 +10,24 @@ import reducer from './reducer';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+// Testing functional components that use useDispatch and useSelector hooks 
+// can be slightly different than regular connected component testing. 
+// This test setup demonstrates a foolproof way of testing components that works for 
+// both kinds of components (components that are using these hooks or connected components).
+
+// our component depends on the redux store, we must also wrap it in a Provider HOC, 
+// exported by react-redux. And since the purpose of this tutorial is testing our component fully, 
+// including the redux side, we must create a 'mock store' for our redux <Provider /> with 
+// an initial state that satisfies the structure of our reducer. Once that’s done, 
+// we can render our component using 'mount'
+
 describe('<Component /> unit test', () => {
-  const getWrapper = (mockStore = createStore(reducer, { count: 0 })) => mount(
-    <Provider store={mockStore}>
-      <Component/>
-    </Provider>
-  );
+  const getWrapper = (mockStore = createStore(reducer, { count: 0 })) =>
+    mount(
+      <Provider store={mockStore}>
+        <Component />
+      </Provider>
+    );
 
   it('should add to count and display the correct # of counts', () => {
     const wrapper = getWrapper();
